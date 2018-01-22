37ea2c954692fbd74ed92e30a69005a25a982711
joinmouse <908380434@qq.com>
2018-01-22 23:26:16 +0800

    拆分组价

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,44 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import TodoInput from './Todo/TodoInput'
-import TodoItem from './Todo/TodoItem'
-import UserDialog from './Dialog/User'
-import {getCurrentUser, signOut, TodoModel} from './Db/leanCloud'
+//import TodoInput from './Todo/TodoInput'
+//import TodoItem from './Todo/TodoItem'
+//import UserDialog from './Dialog/User'
+//import {getCurrentUser, signOut, TodoModel} from './Db/leanCloud'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      user: getCurrentUser() || {},
-      newTodo: '',
-      todoList: []
-    }
-
-    let user = getCurrentUser()
-    if(user) {
-      TodoModel.getByUser(user, (todos) => {
-        let stateCopy = JSON.parse(JSON.stringify(this.state))
-        stateCopy.todoList = todos
-        this.setState(stateCopy)
-      })
+      newTodo: 'test',
+      todoList: [
+        {id:1, title:'第一个待办'}
+      ]
     }
   }
 
-  
+  //渲染
   render() {
+    let todos = this.state.todoList.map((item, index) => {
+      return <li>{item.title}</li>
+    })
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+          <input type="text" value={this.state.newTodo} />
+        </div>
+        <ol>
+          {todos}
+        </ol>
       </div>
-    );
+    )
   }
+
 }
 
 export default App;
